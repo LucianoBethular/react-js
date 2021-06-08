21c49f86ffb9dc74f0ebe1f47ec983792eaaa504
LucianoBethular <luciano.b3@hotmail.com>
2021-06-08 20:22:29 -0300

    se agrego componente

--- src/App.js
+++ src/App.js
@@ -1,43 +1,18 @@
-import React, { Component , Fragment} from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap/dist/js/bootstrap.js';
-import './App.css'
+import React, { Component } from 'react';
+import NavBar from './components/NavBar';
+import './App.css';
+
+
+
+
 
 class App extends Component { 
 render (){
   return (
-    <Fragment>
-      <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-  <div className="container-fluid">
-    <a className="navbar-brand" href="#">BB-TECHNO</a>
-    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span className="navbar-toggler-icon"></span>
-    </button>
-    <div className="collapse navbar-collapse" id="navbarNav">
-      <ul className="navbar-nav">
-        <li className="nav-item">
-          <a className="nav-link active" aria-current="page" href="#">Inicio</a>
-        </li>
-        <li className="nav-item dropdown">
-          <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Productos
-          </a>
-          <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
-            <li><a className="dropdown-item" href="#">Teclados</a></li>
-            <li><a className="dropdown-item" href="#">Auriculares</a></li>
-            <li><a className="dropdown-item" href="#">Mouse / Mousepad</a></li>
-            <li><a className="dropdown-item" href="#">Sillas</a></li>
-          </ul>
-        </li>
-        <li className="nav-item">
-          <a className="nav-link" href="#">Contacto</a>
-        </li>
-      </ul>
+    <div className="app">
+    <NavBar />
+  
     </div>
-  </div>
-</nav>
-    </Fragment>
   );
 }
 }
---
+++ src/components/navBar.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/js/bootstrap.js';
+
+class NavBar extends Component {
+    render() {
+        return (
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+<div className="container-fluid">
+    <a className="navbar-brand" href="#">BB-TECHNO</a>
+    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span className="navbar-toggler-icon"></span>
+    </button>
+    <div className="collapse navbar-collapse" id="navbarNav">
+    <ul className="navbar-nav">
+        <li className="nav-item">
+        <a className="nav-link active" aria-current="page" href="#">Inicio</a>
+        </li>
+        <li className="nav-item dropdown">
+        <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Productos
+        </a>
+        <ul className="dropdown-menu dropdown-menu-dark" aria-labelledby="navbarDropdownMenuLink">
+            <li><a className="dropdown-item" href="#">Teclados</a></li>
+            <li><a className="dropdown-item" href="#">Auriculares</a></li>
+            <li><a className="dropdown-item" href="#">Mouse / Mousepad</a></li>
+            <li><a className="dropdown-item" href="#">Sillas</a></li>
+        </ul>
+        </li>
+        <li className="nav-item">
+        <a className="nav-link" href="#">Contacto</a>
+        </li>
+    </ul>
+    </div>
+</div>
+</nav>
+        )
+    }
+};
+
+
+export default NavBar;
